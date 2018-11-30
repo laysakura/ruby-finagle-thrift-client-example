cdaaa5d89dd577c52a40c246bdf01f85ed109c3d
Sho Nakatani <lay.sakura@gmail.com>
2018-11-30 17:00:35 +0900

    client-name 修正

--- run.rb
+++ run.rb
@@ -4,6 +4,7 @@ require 'finagle-thrift'
 require 'hello_service'
 
 SERVER = 'localhost:9999'
+CLIENT_NAME = 'ruby-finagle-thrift-client-example'
 
 # HelloServiceのクライアント作成
 client = ::ThriftClient.new(::Laysakura::Idl::HelloService::Client, SERVER)
@@ -14,8 +15,8 @@ client = ::ThriftClient.new(::Laysakura::Idl::HelloService::Client, SERVER)
 ##
 ## このように設定しないと、 https://github.com/twitter/finagle/blob/version-6.40.0/finagle-thrift/src/main/ruby/lib/finagle-thrift/trace.rb#L145 のように
 ## gethostname(3) の結果を名前解決しようとするが、名前解決できない名前が取得されるとThrift API callが例外で失敗する。
-::Trace.default_endpoint = ::Trace::Endpoint.new(::Trace::Endpoint.host_to_i32('127.0.0.1'), 0, 'finagle-ruby')
-::FinagleThrift.enable_tracing!(client, ::FinagleThrift::ClientId.new(name: 'ruby-finagle-thrift-client-example'))
+::Trace.default_endpoint = ::Trace::Endpoint.new(::Trace::Endpoint.host_to_i32('127.0.0.1'), 0, CLIENT_NAME)
+::FinagleThrift.enable_tracing!(client, ::FinagleThrift::ClientId.new(name: CLIENT_NAME))
 
 # HelloServiceのサーバ側の sayHelloTo() APIを叩く
 begin
